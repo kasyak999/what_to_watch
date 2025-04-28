31d3826e3841558ce1ed817c52713684deab1452
Максим <kasyak999@yandex.ru>
2025-04-28 21:57:57 +0300

    up

--- opinions_app.py
+++ opinions_app.py
@@ -1,19 +1,27 @@
 from datetime import datetime
 
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, url_for, flash, abort
 from pprint import pprint
 # Импортируется нужный класс:
 from flask_sqlalchemy import SQLAlchemy
 # Импортируется функция для выбора случайного значения:
 from random import randrange
 
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField, URLField
+from wtforms.validators import DataRequired, Length, Optional
+
+from flask_migrate import Migrate
+
 
 app = Flask(__name__, static_folder='static')
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SECRET_KEY'] = 'MY SECRET KEY'
 # Создаётся экземпляр класса SQLAlchemy и передаётся 
 # в качестве параметра экземпляр приложения Flask:
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 class Opinion(db.Model):
@@ -31,15 +39,56 @@ class Opinion(db.Model):
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
 
+# Класс формы опишите сразу после модели Opinion.
+class OpinionForm(FlaskForm):
+    title = StringField(
+        'Введите название фильма',
+        validators=[DataRequired(message='Обязательное поле'),
+                    Length(1, 128)]
+    )
+    text = TextAreaField(
+        'Напишите мнение',
+        validators=[DataRequired(message='Обязательное поле')]
+    )
+    source = URLField(
+        'Добавьте ссылку на подробный обзор фильма',
+        validators=[Length(1, 256), Optional()]
+    )
+    submit = SubmitField('Добавить')
+
+
+# Тут декорируется обработчик и указывается код нужной ошибки:
+@app.errorhandler(500)
+def internal_error(error):
+    # Ошибка 500 возникает в нештатных ситуациях на сервере. 
+    # Например, провалилась валидация данных.
+    # В таких случаях можно откатить изменения, не зафиксированные в БД,
+    # чтобы в базу не записалось ничего лишнего.
+    db.session.rollback()
+    # Пользователю вернётся страница, сгенерированная на основе шаблона 500.html.
+    # Этого шаблона пока нет, но сейчас мы его тоже создадим.
+    # Пользователь получит и код HTTP-ответа 500.
+    return render_template('500.html'), 500
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    # При ошибке 404 в качестве ответа вернётся страница, созданная
+    # на основе шаблона 404.html, и код HTTP-ответа 404:
+    return render_template('404.html'), 404
+
+
 @app.route('/')
 def index_view():
     # pprint(app.config)
     # Определяется количество мнений в базе данных:
     quantity = Opinion.query.count()
+    # quantity = None
     # Если мнений нет...
     if not quantity:
         # ...то возвращается сообщение:
-        return 'В базе данных мнений о фильмах нет.'
+        # return 'В базе данных мнений о фильмах нет.'
+        abort(500)
     # Иначе выбирается случайное число в диапазоне от 0 до quantity...
     offset_value = randrange(quantity)
     # ...и определяется случайный объект:
@@ -48,9 +97,46 @@ def index_view():
     return render_template('opinion.html', opinion=opinion)
 
 
-@app.route('/add')
+@app.route('/add', methods=['GET', 'POST'])
 def add_opinion_view():
-    return render_template('add_opinion.html')
+    # Объект формы передаётся в шаблон add_opinion.html:
+    # return render_template('add_opinion.html', form=form)
+    form = OpinionForm()
+    # Если ошибок не возникло...
+    if form.validate_on_submit():
+        text = form.text.data
+        # Если в БД уже есть мнение с текстом, который ввёл пользователь...
+        if Opinion.query.filter_by(text=text).first() is not None:
+            # ...вызвать функцию flash и передать соответствующее сообщение:
+            flash('Такое мнение уже было оставлено ранее!', 'free-message')
+            # Вернуть пользователя на страницу «Добавить новое мнение»:
+            return render_template('add_opinion.html', form=form)
+        
+        # ...то нужно создать новый экземпляр класса Opinion:
+        opinion = Opinion(
+            # И передать в него данные, полученные из формы:
+            title=form.title.data,
+            text=form.text.data,
+            source=form.source.data
+        )
+        # Затем добавить его в сессию работы с базой данных:
+        db.session.add(opinion)
+        # И зафиксировать изменения:
+        db.session.commit()
+        # Затем переадресовать пользователя на страницу добавленного мнения:
+        return redirect(url_for('opinion_view', id=opinion.id))
+    # Если валидация не пройдена — просто отрисовать страницу с формой:
+    return render_template('add_opinion.html', form=form)
+
+
+# Тут указывается конвертер пути для id:
+@app.route('/opinions/<int:id>')
+# Параметром указывается имя переменной:
+def opinion_view(id):
+    # Теперь можно запросить нужный объект по id...
+    opinion = Opinion.query.get_or_404(id)
+    # ...и передать его в шаблон (шаблон тот же, что и для главной страницы):
+    return render_template('opinion.html', opinion=opinion)
 
 
 if __name__ == '__main__':
